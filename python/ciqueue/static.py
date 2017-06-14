805517aff021f8922b0772b5dd850cc4b97cb21c
Jean Boussier <jean.boussier@gmail.com>
2017-06-14 14:47:35 -0400

    Implement static and file queues in Python

---
+++ python/ciqueue/static.py
@@ -0,0 +1,31 @@
+import math
+
+class Static(object):
+  def __init__(self, tests, max_requeues=0, requeue_tolerance=0):
+    self.queue = tests
+    self.progress = 0
+    self.total = len(tests)
+    self.max_requeues = max_requeues
+    self.global_max_requeues = math.ceil(requeue_tolerance * self.total)
+    self.requeues = {}
+
+  def __len__(self):
+    return len(self.queue)
+
+  def __iter__(self):
+    while self.queue:
+      yield self.queue.pop(0)
+      self.progress += 1
+
+  def acknowledge(self, test):
+    return True
+
+  def requeue(self, test):
+    if self.requeues.get(test, 0) >= self.max_requeues:
+      return False
+    if sum(self.requeues.values()) >= self.global_max_requeues:
+      return False
+
+    self.requeues[test] = self.requeues.get(test, 0) + 1
+    self.queue.insert(0, test)
+    return True
---
+++ python/tests/test_static.py
@@ -0,0 +1,7 @@
+from ciqueue import Static
+from shared import QueueImplementation
+
+
+class TestStatic(QueueImplementation):
+  def build_queue(self):
+    return Static(list(self.TEST_LIST), max_requeues=1, requeue_tolerance=0.1)
