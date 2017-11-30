26ce59969e432cb33db95192ae2e5094ca9baebe
Jean Boussier <jean.boussier@gmail.com>
2017-11-30 17:15:42 +0100

    Ruby release 0.9.1

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -1,6 +1,6 @@
 module CI
   module Queue
-    VERSION = '0.9.0'
+    VERSION = '0.9.1'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
