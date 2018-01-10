72121a9401f4efff05c5bbc04ea26168fe9e20c7
Jean Boussier <jean.boussier@gmail.com>
2018-01-10 17:06:22 +0100

    Ruby release 0.10.1

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -1,6 +1,6 @@
 module CI
   module Queue
-    VERSION = '0.10.0'
+    VERSION = '0.10.1'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
