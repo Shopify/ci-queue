356a5c2c2b55c38d7cac2d4b51f551d36ee40245
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2018-06-25 12:14:57 -0400

    Ruby release 0.13.4

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -1,6 +1,6 @@
 module CI
   module Queue
-    VERSION = '0.13.3'
+    VERSION = '0.13.4'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
