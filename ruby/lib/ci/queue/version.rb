e80cc2e0941b274c804e807a55c9057163c1a405
Jean Boussier <jean.boussier@gmail.com>
2018-07-27 14:32:07 -0400

    Ruby release 0.14.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -1,6 +1,6 @@
 module CI
   module Queue
-    VERSION = '0.13.6'
+    VERSION = '0.14.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
