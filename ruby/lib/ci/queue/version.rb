9834e3e07916ba69369ea42b457b7383e4624269
Doug Edey <doug.edey@shopify.com>
2019-03-05 09:03:35 -0500

    Release 0.15.1

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -1,6 +1,6 @@
 module CI
   module Queue
-    VERSION = '0.15.0'
+    VERSION = '0.15.1'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
