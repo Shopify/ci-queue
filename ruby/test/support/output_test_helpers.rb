d4ac6a74830b161964b98a88f169292deb950ac5
Willem van Bergen <willem@vanbergen.org>
2020-05-06 16:36:03 -0400

    Rewrite JunitReporter.
    - Use REXML rather than Builder. Slightly more verbose, but doesn’t turn escape codes into garbage. Also means that we can potentially get rid of a dependency.
    - Wrap output in CDATA sections so we don’t have to format it according to XML’s rules.
    - Add unit tests.

--- ruby/test/support/output_test_helpers.rb
+++ ruby/test/support/output_test_helpers.rb
@@ -29,7 +29,7 @@ module OutputTestHelpers
   end
 
   def freeze_xml_timing(output)
-    output.gsub(/time="[\d\-\.e]+"/, 'time="X.XX"')
+    output.gsub(/time='[\d\-\.e]+'/, "time='X.XX'")
   end
 
   def normalize(output)
--- ruby/test/support/reporter_test_helper.rb
+++ ruby/test/support/reporter_test_helper.rb
@@ -2,21 +2,42 @@
 module ReporterTestHelper
   private
 
-  def result(*args, **kwargs)
-    result = runnable(*args, **kwargs)
-    if defined? Minitest::Result
-      result = Minitest::Result.from(result)
-    end
+  def result(name, **kwargs)
+    result = Minitest::Result.from(runnable(name, **kwargs))
+    result.source_location = ["test/my_test.rb", 12]
     result
   end
 
   def runnable(name, failure: nil, requeued: false, skipped: false, unexpected_error: false)
     runnable = Minitest::Test.new(name)
-    runnable.failures << failure if failure
+    runnable.failures << generate_assertion(failure) if failure
     runnable.failures << MiniTest::Skip.new if skipped
-    runnable.failures << Minitest::UnexpectedError.new(StandardError.new) if unexpected_error
-    runnable.failures << MiniTest::Requeue.new('Failed') if requeued
+    runnable.failures << generate_unexpected_error if unexpected_error
+    runnable.failures << MiniTest::Requeue.new(generate_assertion("Failed")) if requeued
     runnable.assertions += 1
+    runnable.time = 0.12
     runnable
+  end
+
+  def generate_unexpected_error
+    error = StandardError.new
+    error.set_backtrace([
+      "#{Dir.pwd}/test/support/reporter_test_helper.rb:15:in `runnable'",
+      "#{Dir.pwd}/test/support/reporter_test_helper.rb:6:in `result'",
+      "#{Dir.pwd}/test/minitest/reporters/junit_reporter_test.rb:65:in `test_generate_junitxml_for_errored_test'",
+    ])
+    Minitest::UnexpectedError.new(error)
+  end
+
+  def generate_assertion(message)
+    return message if message.is_a?(Minitest::Assertion)
+
+    error = Minitest::Assertion.new(message)
+    error.set_backtrace([
+      "#{Dir.pwd}/test/support/reporter_test_helper.rb:15:in `runnable'",
+      "#{Dir.pwd}/test/support/reporter_test_helper.rb:6:in `result'",
+      "#{Dir.pwd}/test/minitest/reporters/junit_reporter_test.rb:65:in `test_generate_junitxml_for_errored_test'",
+    ])
+    error
   end
 end
