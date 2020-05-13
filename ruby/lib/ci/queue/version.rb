b5699877cb0c10c8b8c7b035238db27b20d5df8c
Willem van Bergen <willem@vanbergen.org>
2020-05-13 12:46:06 -0400

    Bump Ruby version to 0.20.1

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.20.0'
+    VERSION = '0.20.1'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
