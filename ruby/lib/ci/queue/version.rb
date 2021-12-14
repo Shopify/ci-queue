fe6135c28a9e47f7396d9eca39671aad4e2f020e
Jean Boussier <jean.boussier@gmail.com>
2021-12-14 13:50:30 +0100

    Ruby release 0.22.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.21.1'
+    VERSION = '0.22.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
