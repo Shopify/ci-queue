5454b4420057d0946a954d3cb3e36dd43144359c
Jean Boussier <jean.boussier@gmail.com>
2023-03-10 16:39:06 +0100

    Ruby release 0.25.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.24.3'
+    VERSION = '0.25.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
