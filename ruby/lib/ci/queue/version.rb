aa16b8798b23cffe2b657a555e48b3de928ca634
Christian Bruckmayer <christian.bruckmayer@shopify.com>
2023-03-15 08:57:01 +0000

    Release 0.26.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.25.0'
+    VERSION = '0.26.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
