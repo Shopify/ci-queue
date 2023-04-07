bab916bc8a082e6a0ae35a748a61efe5ff231004
Randy Stauner <randy.stauner@shopify.com>
2023-04-07 15:08:40 -0700

    Use REDIS_URL instead of REDIS_HOST to support alternate ports

--- ruby/test/ci/queue/redis_test.rb
+++ ruby/test/ci/queue/redis_test.rb
@@ -5,7 +5,7 @@ class CI::Queue::RedisTest < Minitest::Test
   include SharedQueueAssertions
 
   def setup
-    @redis_url = "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}/7"
+    @redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
     @redis = ::Redis.new(url: @redis_url)
     @redis.flushdb
     super
