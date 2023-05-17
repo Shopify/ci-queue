6e8be89bf20f413964a6c7ed3f303f024e816626
Nikita Acharya <nikita.acharya@shopify.com>
2023-05-17 09:07:03 -0700

    Release 0.28.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.27.0'
+    VERSION = '0.28.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
