55e662a4071e6f1dfef53a7bbe9b080f2a49db75
Randy Stauner <randy.stauner@shopify.com>
2023-06-13 13:49:02 -0700

    Support multiple -I args
    closes #157

--- ruby/lib/minitest/queue/runner.rb
+++ ruby/lib/minitest/queue/runner.rb
@@ -414,7 +414,7 @@ module Minitest
           EOS
           opts.separator ""
           opts.on('-IPATHS', help) do |paths|
-            self.load_paths = paths
+            self.load_paths = [load_paths, paths].compact.join(':')
           end
 
           help = <<~EOS
