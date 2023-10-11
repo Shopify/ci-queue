77d83d1b103543426eece40fc8ae22c1852c5b07
Nikita Acharya <nikita.acharya@shopify.com>
2023-10-11 09:29:51 -0700

    Fix flaky test record for skipped test

--- ruby/test/minitest/queue/build_status_recorder_test.rb
+++ ruby/test/minitest/queue/build_status_recorder_test.rb
@@ -17,6 +17,7 @@ module Minitest::Queue
     def test_aggregation
       @reporter.record(result('a', failure: "Something went wrong"))
       @reporter.record(result('b', unexpected_error: true))
+      @reporter.record(result('h', failure: "Something went wrong", requeued: true))
 
       second_queue = worker(2)
       second_reporter = BuildStatusRecorder.new(build: second_queue.build)
@@ -27,13 +28,15 @@ module Minitest::Queue
       second_reporter.record(result('e', skipped: true))
       second_reporter.record(result('f', unexpected_error: true))
       second_reporter.record(result('g', requeued: true))
+      second_reporter.record(result('h', skipped: true, requeued: true))
 
-      assert_equal 7, summary.assertions
-      assert_equal 2, summary.failures
+      assert_equal 9, summary.assertions
+      assert_equal 3, summary.failures
       assert_equal 3, summary.errors
-      assert_equal 1, summary.skips
+      assert_equal 2, summary.skips
       assert_equal 1, summary.requeues
       assert_equal 5, summary.error_reports.size
+      assert_equal 0, summary.flaky_reports.size
     end
 
     def test_retrying_test
