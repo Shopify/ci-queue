ae3a4aaeeeb3e265503a98cb80726e3f613c61b7
stephsachrajda <steph.sachrajda@shopify.com>
2023-10-19 15:37:57 -0600

    try removing abort

--- ruby/lib/minitest/queue/runner.rb
+++ ruby/lib/minitest/queue/runner.rb
@@ -231,7 +231,7 @@ module Minitest
             if supervisor.max_test_failed?
               puts("Encountered #{supervisor.test_failed} failures")
               puts('Encountered too many failed tests. Test run was ended early.')
-              abort!(msg)
+              # abort!(msg)
             else
               abort!(msg)
             end
