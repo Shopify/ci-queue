032e6208d6da1b9dfac72ceec7ada549edf909c3
Christian Bruckmayer <christian.bruckmayer@shopify.com>
2024-01-30 13:57:48 +0000

    Bump version

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.43.0'
+    VERSION = '0.44.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
