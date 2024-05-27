ef7c5ae3e740ab9cfbc2e440d814e7368b87d047
Zarif Mahfuz <zarif.mahfuz@shopify.com>
2024-05-27 15:09:12 -0600

    Bump version

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.53.0'
+    VERSION = '0.54.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
