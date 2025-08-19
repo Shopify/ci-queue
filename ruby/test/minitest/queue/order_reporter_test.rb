ef14fca166a8e8ddb72b61090d1131f71ab3b57c
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2025-08-19 13:19:01 -0500

    Make the CI Queue reporters work in parallel
    Make Minitest::Queue::OrderReporter parallel-friendly. This is
    essentially done by appending and truncating to the log file instead of
    writing to it. The object also doesn't depend on the start hook anymore
    to initialize the log file, because that isn't called in forked workers.

--- ruby/test/minitest/queue/order_reporter_test.rb
+++ ruby/test/minitest/queue/order_reporter_test.rb
@@ -7,14 +7,37 @@ module Minitest::Queue
 
     def setup
       @reporter = OrderReporter.new(path: log_path)
+    end
+
+    def test_start
       @reporter.start
+      @reporter.report
+      assert_equal [], File.readlines(log_path).map(&:chomp)
     end
 
     def test_before_test
+      @reporter.start
       @reporter.before_test(runnable('a'))
       @reporter.before_test(runnable('b'))
       @reporter.report
       assert_equal ['Minitest::Test#a', 'Minitest::Test#b'], File.readlines(log_path).map(&:chomp)
+    end
+
+    def test_forking
+      pid = fork do
+        @reporter.start
+      end
+      pids = 5.times.map do
+        fork do
+          @reporter.before_test(runnable(Process.pid))
+          @reporter.report
+        end
+      end
+      (pids + [pid]).map do |pid|
+        Process.waitpid(pid)
+      end
+
+      assert_equal pids.map { |pid| "Minitest::Test##{pid}" }.sort, File.readlines(log_path).map(&:chomp).sort
     end
 
     private
