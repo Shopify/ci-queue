5ca14b327af574b11d014adf9a09cfdfc7460e21
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2026-04-07 17:08:44 -0400

    Bump to v0.88.0

--- ruby/lib/ci/queue/version.rb
+++ ruby/lib/ci/queue/version.rb
@@ -2,7 +2,7 @@
 
 module CI
   module Queue
-    VERSION = '0.87.0'
+    VERSION = '0.88.0'
     DEV_SCRIPTS_ROOT = ::File.expand_path('../../../../../redis', __FILE__)
     RELEASE_SCRIPTS_ROOT = ::File.expand_path('../redis', __FILE__)
   end
